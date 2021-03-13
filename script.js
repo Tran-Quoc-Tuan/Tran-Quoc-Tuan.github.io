f09a31390f711ef8f923cecdfb400c71225c5300
Tran-Quoc-Tuan <55978031+Tran-Quoc-Tuan@users.noreply.github.com>
2021-03-13 21:11:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var request = new XMLHttpRequest();
 request.open("GET", "https://limitless-bastion-11970.herokuapp.com/");
 try {
+  console.log(request.readyState);
   request.send();
+  console.log("Success");
 }
 catch(err) {
   console.log(err.message);
