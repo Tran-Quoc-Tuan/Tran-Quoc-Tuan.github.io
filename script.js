5e9a037f8200eee749245c3fa7dc93fbe825e817
Tran-Quoc-Tuan <55978031+Tran-Quoc-Tuan@users.noreply.github.com>
2021-03-20 14:43:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 console.log("Hello");
 var request = new XMLHttpRequest();
-request.open("GET", "https://limitless-bastion-11970.herokuapp.com/Ảnh/");
+request.open("GET", "https://limitless-bastion-11970.herokuapp.com/thumuc/");
 try {
   console.log(request.readyState);
   request.send();
+  console.log(request.responseText);
   console.log("Success");
 }
 catch(err) {
