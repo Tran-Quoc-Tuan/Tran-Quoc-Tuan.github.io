cc86c34868b2c579069fe5ea01fd5fd4f4a5f7ea
Tran-Quoc-Tuan <tranquoct700@gmail.com>
2021-03-20 15:10:46 +0700

    ab

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.write("Hello world");
-console.log("Hello");
+
 var request = new XMLHttpRequest();
-request.open("GET", "https://limitless-bastion-11970.herokuapp.com/thumuc/");
+request.open("GET", "https://limitless-bastion-11970.herokuapp.com/");
 try {
   console.log(request.readyState);
+  request.setRequestHeader('Access-Control-Allow-Origin', '*');
   request.send();
   console.log(request.responseText);
   console.log("Success");
